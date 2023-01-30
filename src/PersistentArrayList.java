35d642bb4b27afe39310995603937ee20b1d009a
Ben Tetteh <ben@Bens-MacBook-Pro.local>
2023-01-30 09:45:23 +0000

    Initial commit

---
+++ src/PersistentArrayList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class PersistentArrayList {
+
+    private ArrayList<String> names;
+
+    PersistentArrayList() {
+    }
+
+    public String get(int i){
+        String name = names.get(i);
+
+        return name;
+    }
+
+    public vid
+}
